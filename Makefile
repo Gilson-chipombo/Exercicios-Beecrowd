5cb54eb9d56d875d14dc80bddb03c18db94d98c7
Gilson-chipombo <fernandochipombo@gmail.com>
2025-10-31 12:40:23 +0100

    begin, rebegin

---
+++ Makefile
@@ -0,0 +1,14 @@
+CXX = c++
+CXXFLAGS = -Wall -std=c++17
+SRC = trainString.cpp
+OBJS = $(SRC:.cpp=.o)
+NAME = trainString
+
+all: $(NAME)
+$(NAME): $(OBJS)
+	$(CXX) $(CXXFLAGS) -o $(NAME) $(OBJS)
+clean: 
+	rm -f $(OBJS)
+fclean: clean
+	rm -f $(NAME)
+re: fclean all
---
+++ trainString.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    { 
+        /*
+        string str="Gilson";
+        auto f = str.begin();
+        auto l = str.end()-1;
+        cout << "First Char: "<< *f << endl;
+        cout << "last Char: "<< *l << endl;
+
+        for(auto it = str.begin(); it != str.end(); ++it) cout << *it;
+        cout << endl;
+
+        for(auto it = str.rbegin(); it != str.rend(); ++it) cout << *it;
+        cout<<endl;
+
+        cout << str.max_size() << endl; //Retorna: o tamanho máximo teórico que a string pode atingir no sistema.
+
+        str.resize(3);
+        cout << str << endl;
+
+        str.resize(6, '!');
+        cout << str << endl;
+        */
+        /*
+            capacity()
+            Retorna: a quantidade de memória atualmente alocada (em número de caracteres).
+            Pode ser maior ou igual a size() — a string reserva espaço extra para evitar realocações frequentes.
+        */
+       // cout << str.capacity() << endl;
+
+        /*
+            clear()
+            Apaga todo o conteúdo da string, deixando-a vazia (size() == 0)
+        */
+        //str.clear();
+        //cout << str.empty() << endl;
+    }
+
+  
+    return 0;
+}

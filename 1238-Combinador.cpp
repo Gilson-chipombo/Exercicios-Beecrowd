2e02a8033c642a99b79b2f0f413dfed3f992eb2d
Gilson-chipombo <fernandochipombo@gmail.com>
2025-11-06 22:36:15 +0100

    1238 solved

--- 1238-Combinador.cpp
+++ 1238-Combinador.cpp
@@ -7,21 +7,29 @@ int main(){
     while(q--)
     {
         string s1, s2;
-        getline(cin, s1);
-        getline(cin, s2);
+        cin >> s1;
+        cin >> s2;
         size_t maxLen = max(s1.size(), s2.size());
-        if (s1.size() > maxLen) maxLen = s2.size() - maxLen;
-        else maxLen =  maxLen - s1.size();
+        bool maxS = false;
+        if (s1.size() > s2.size()){
+            maxLen = s1.size() - s2.size();
+            maxS = true;
+        }else maxLen =  s2.size() - s1.size();
         
         string newS;
         if (maxLen > 0){
-            for (size_t i = 0; i < maxLen; i++)
-                 newS.append(s1.at(i)+""+s2.at(i));
-            newS.append(s2.substr(maxLen));
+            for (size_t i = 0; i < min(s1.size(), s2.size()); i++){
+                newS.append(string(1, s1.at(i)));
+                newS.append(string(1, s2.at(i)));
+            }
+            if (maxS) newS.append(s1.substr(min(s1.size(), s2.size())));
+            else newS.append(s2.substr(min(s1.size(), s2.size())));
         }else
         {
-            for (size_t i = 0; i < s1.size(); i++)
+            for (size_t i = 0; i < s1.size(); i++){
                 newS.append(string(1, s1.at(i)));
+                newS.append(string(1, s2.at(i)));
+            }
         }
         cout << newS << endl;
     }

efc05beb0314d88bba1c997ce0b644f9661909fa
Gilson-chipombo <fernandochipombo@gmail.com>
2025-11-09 22:32:20 +0100

    1253

--- 1253-Cifra-de-Cesar.cpp
+++ 1253-Cifra-de-Cesar.cpp
@@ -10,13 +10,13 @@ int main(){
         cin >> s >> salto;
         for (size_t i = 0; i < s.size(); i++)
         {
-            //if (s[i] + salto < 90){
+            if (s[i] + salto < 90){
                 if (s[i] + salto >= 74 && s[i] < 77) s[i] = s[i] + salto + 6;
                 else s[i] = s[i] + salto; 
-            /*}else{
+            }else{
                 if (s[i] - salto < 79) s[i] = s[i] - salto + 6;
                 else s[i] = s[0] - salto; 
-            } */   
+            }   
             cout << s[i];          
         }
         cout << endl;
